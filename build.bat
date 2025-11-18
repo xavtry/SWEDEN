b5cf75f367cfa1a69ba67a7804b971c5f916ce5a
xavtry <sebastianishot68@gmail.com>
2025-11-18 18:42:16 -0500

    Modify build.bat for Emscripten compilation
    Updated build.bat to compile with Emscripten and output sverige.js.

--- build.bat
+++ build.bat
@@ -1,1 +1,11 @@
+emcc main.cpp imgui/*.cpp -Iimgui -I. ^
+  -s USE_SDL=2 -s USE_WEBGL2=1 -s FULL_ES3=1 ^
+  -s WASM=1 -s ALLOW_MEMORY_GROWTH=1 ^
+  -s EXPORTED_FUNCTIONS="['_main']" ^
+  -s EXPORTED_RUNTIME_METHODS="['ccall','cwrap']" ^
+  -O3 --shell-file shell_minimal.html ^
+  -o sverige.js
 
+echo.
+echo SVERIGE.EXE COMPILED! Open index.html
+pause
